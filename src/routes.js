0a0df03b31779fe16f37480d355b6d4f756ea2a0
Luana Mozzer <luanamozzer@MacBook-Luana.local>
2023-01-17 20:54:26 -0300

    criando primeiro controller de users e routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+
+const UserController = require('./controllers/UserController');
+
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: '/products',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+]
