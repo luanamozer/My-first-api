495b33fdb3fd82fbe82e9fa1271e1e623e281609
Luana Mozzer <luanamozzer@MacBook-Luana.local>
2023-01-16 21:05:37 -0300

    criando primeira rota da api

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const users = require('./mocks/users');
+
+const server = http.createServer((request, response) =>{
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if(request.url === '/users' && request.method === 'GET'){
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+  }else{
+    response.writeHead(404, { 'Content-Type': 'application/json'});
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+
+});
+
+server.listen(3333,()=> console.log('Server started at http://localhost:3333'));
---
+++ src/mocks/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    id:1,
+    name:'Pastel',
+  },
+  {
+    id:2,
+    name:'Dori',
+  },
+  {
+    id:3,
+    name:'Deusa',
+  },
+
+];
